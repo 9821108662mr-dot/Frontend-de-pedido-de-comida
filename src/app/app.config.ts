02d995360d74229e1f79048d39556bbd7011952d
JaimeAMR <9821108662mr@gmail.com>
2026-04-26 00:17:47 -0600

    feat(pwa): integración de Service Worker para soporte offline e instalación nativa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { cachingInterceptor } from './interceptors/caching.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptors([cachingInterceptor])),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
