e61548a1e2994934fa3f3d83e6fc69da628cbe3b
JaimeAMR <9821108662mr@gmail.com>
2026-04-26 00:55:24 -0600

    feat(i18n): soporte multi-idioma (es/en) integrado con ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,33 +1,23 @@
-import { ApplicationConfig, isDevMode, importProvidersFrom } from '@angular/core';
-import { HttpClient, provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { cachingInterceptor } from './interceptors/caching.interceptor';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './i18n/', '.json');
-}
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptors([cachingInterceptor]), withFetch()),
     provideRouter(routes),
     provideAnimationsAsync(),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        defaultLanguage: 'es',
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
-      })
-    ),
+    provideTranslateService({
+      fallbackLang: 'es'
+    }),
+    provideTranslateHttpLoader(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
